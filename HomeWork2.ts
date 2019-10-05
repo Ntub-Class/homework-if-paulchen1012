2601e260a250de0df8d9e683b6495674d94dd75f
bobo <N1086717@ntub.edu.tw>
2019-10-06 00:09:14 +0800

    HomeWork2

---
+++ HomeWork2.ts
@@ -0,0 +1,41 @@
+//介紹兩個String的用法
+//第一個String.prototype.substr()
+//範例
+let str = 'Mozilla';
+console.log(str.substr(1, 2));
+//解釋
+//從第一個字之後開始取兩個字，輸出結果=oz
+//------------------------------------------------------------------------------------
+//第二個String.replace()
+//範例
+let a = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+console.log(a.replace('dog', 'monkey'));
+//解釋
+//把字串中的dog改成monkey
+//輸出結果=he quick brown fox jumps over the lazy monkeys. If the dog reacted, was it 
+
+
+//------------------------------------------------------------------------------------
+
+
+//介紹兩個Number的用法
+//第一個Number.isInteger()
+//範例
+function fits(x, y) {
+    if (Number.isInteger(y / x)) {
+        return 'Fits!';
+    }
+    return 'Does NOT fit!';
+}
+//解釋
+//如果目標值為整數，則返回true，否則返回false。
+//如果值為NaNor Infinity，則返回false。該方法還將返回true 可表示為整數的浮點數。
+//------------------------------------------------------------------------------------
+//第二個"!=="
+//範例 
+6 !== 2 + 3;
+//解釋
+//!==用來判斷左邊的值跟右邊的值是否不相同
+//結果為true
+
+
